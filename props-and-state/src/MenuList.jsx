3b372afc2c8938f60aca0024078cbf5374eab9de
ATN <contact.atn35@gmail.com>
2024-05-22 21:59:24 +0200

    Exercice React - Props and State

---
+++ props-and-state/src/MenuList.jsx
@@ -0,0 +1,20 @@
+import MenuItem from './MenuItem';
+
+function MenuList({ foodItems }) {
+  return (
+    <>
+      {foodItems.map((item, index) => (
+        <MenuItem
+          key={index}
+          itemName={item.itemName}
+          description={item.description}
+          foodImage={item.foodImage}
+          price={item.price}
+          isFavorite={item.isFavorite}
+        />
+      ))}
+    </>
+  );
+}
+
+export default MenuList;
